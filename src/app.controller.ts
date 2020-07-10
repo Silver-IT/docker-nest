aefcca8cf10eafcb11a6dd8f60a4bf7c5f5ba9cd
silver-it <silver.19920220@gmail.com>
2020-07-09 17:01:37 -0700

    feat: added API endpoint

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -9,4 +9,9 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Get('users')
+  getUsers(): number {
+    return this.appService.getUsers();
+  }
 }
